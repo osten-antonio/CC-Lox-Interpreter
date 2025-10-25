04937236605cf69c50fdf48806ac772a2f563601
osten-antonio <ostenantonio1@gmail.com>
2025-10-25 20:16:41 +0700

    Number literals

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1,21 +1,33 @@
 #include <_Parser.h>
 #include <iostream>
+#include <sstream>
+#include <iomanip>
 
 Parser::Parser(std::vector<Token> _tokens):tokens(_tokens)
 {}
 
+
 void Parser::print(){
     for(const Token &token:tokens){
         switch(token.tokenType){
-            case NIL:
-                std::cout << "nil" << '\n';
+            case NUMBER:
+            {
+                std::ostringstream oss;
+                oss << std::setprecision(15);
+                oss << token.lexeme;
+
+                std::string s = oss.str();
+
+                // Ensure at least 1 decimal point (eg 1 -> 1.0)
+                if (s.find('.') == std::string::npos && s.find('e') == std::string::npos) {
+                    s += ".0";
+                }
+                std::cout << s << '\n';
                 break;
-            case TRUE:
-                std::cout << "true" << '\n';
-                break;
-            case FALSE:
-                std::cout <<"false" << '\n';
-                break;
+            }
+            default:
+                std::cout << token.lexeme << '\n';
+
         }
 
     }
